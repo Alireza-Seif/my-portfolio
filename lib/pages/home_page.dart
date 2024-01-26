5787d0b3e181173685c0756bc0a7af632de1c67f
alirezaseif20 <alirezaariye@gmail.com>
2024-01-26 19:10:57 +0330

    feat: make a responsive header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
 
+import '../constants/size.dart';
 import '../widgets/drawer_mobile.dart';
+import '../widgets/header_desktop.dart';
 import '../widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,44 +18,51 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        key: scaffoldKey,
-        backgroundColor: CustomColor.scaffoldBg,
-        endDrawer: const DrawerMobile(),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            // MAIn
-            //  const HeaderDesktop(),
-            HeaderMobile(
-              onLogoTap: () {},
-              onMenuTap: () {
-                scaffoldKey.currentState?.openEndDrawer();
-              },
-            ),
-            // SKILLS
-            Container(
-              height: 500,
-              width: double.maxFinite,
-              color: Colors.blueGrey,
-            ),
-            // Projects
-            const SizedBox(
-              height: 500,
-              width: double.maxFinite,
-            ),
-            // CONACT
-            Container(
-              height: 500,
-              width: double.maxFinite,
-              color: Colors.blueGrey,
-            ),
-            // FOOTER
-            const SizedBox(
-              height: 500,
-              width: double.maxFinite,
-            )
-          ],
-        ));
+    return LayoutBuilder(builder: (context, Constraints) {
+      return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: CustomColor.scaffoldBg,
+          endDrawer: Constraints.maxWidth >= kMinDesktopWidth
+              ? null
+              : const DrawerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              // MAIn
+              if (Constraints.maxWidth >= kMinDesktopWidth)
+                const HeaderDesktop()
+              else
+                HeaderMobile(
+                  onLogoTap: () {},
+                  onMenuTap: () {
+                    scaffoldKey.currentState?.openEndDrawer();
+                  },
+                ),
+
+              // SKILLS
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              // Projects
+              const SizedBox(
+                height: 500,
+                width: double.maxFinite,
+              ),
+              // CONACT
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              // FOOTER
+              const SizedBox(
+                height: 500,
+                width: double.maxFinite,
+              )
+            ],
+          ));
+    });
   }
 }
