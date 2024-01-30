e07798a705651b0cdee37c76abe380b3aaf39e59
alirezaseif20 <alirezaariye@gmail.com>
2024-01-30 21:39:16 +0330

    feat: add some images for logos

---
+++ lib/constants/skill_items.dart
@@ -0,0 +1,18 @@
+const List<Map> skillItems = [
+  {'img': 'assets/icons/flutter.png', 'title': 'Flutter'},
+  {'img': 'assets/icons/dart.png', 'title': 'Dart'},
+  {'img': 'assets/icons/git.png', 'title': 'Git'},
+  {'img': 'assets/icons/linux.png', 'title': 'Linux'},
+  {'img': 'assets/icons/python.png', 'title': 'Python'},
+  {'img': 'assets/icons/laravel.png', 'title': 'Laravel'},
+  {'img': 'assets/icons/php.png', 'title': 'PHP'},
+  {'img': 'assets/icons/html.png', 'title': 'HTML5'},
+  {'img': 'assets/icons/css.png', 'title': 'CSS3'},
+];
+
+const List<Map> platformItems = [
+  {'img': 'assets/icons/android.png', 'title': 'Android Dev'},
+  {'img': 'assets/icons/apple.png', 'title': 'IOS Dev'},
+  {'img': 'assets/icons/desktop.png', 'title': 'Desktop Dev'},
+  {'img': 'assets/icons/web.png', 'title': 'Web Dev'},
+];
